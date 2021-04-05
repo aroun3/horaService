d935956cdfc541f0a2d9cc9891b94208d5b38ef4
dramanedigin <69799519+dramanedigin@users.noreply.github.com>
2021-04-05 08:30:33 +0000

    commit2 acceuil 05042021

--- src/main/java/com/visitor/repositories/AreaGpsRepository.java
+++ src/main/java/com/visitor/repositories/AreaGpsRepository.java
@@ -3,9 +3,11 @@ package com.visitor.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.visitor.entities.AreaGps;
 
+@Repository
 public interface AreaGpsRepository extends JpaRepository<AreaGps, Integer>{
 
 	List<AreaGps> FindByArea(String area);
