b136cb300be27bab595f45b6febd93db4e5c0f64
moryDev <mory@diginfactory.com>
2021-04-12 10:10:04 +0200

    correction erreur3

--- src/main/java/com/visitor/repositories/PunchHistoryRepository.java
+++ src/main/java/com/visitor/repositories/PunchHistoryRepository.java
@@ -3,6 +3,7 @@ package com.visitor.repositories;
 import java.util.Date;
 import java.util.List;
 
+import com.visitor.entities.HistoryPointage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -80,7 +81,7 @@ public interface PunchHistoryRepository extends JpaRepository<PunchHistory, Inte
 			+ " pp.position_name as position, pd.dept_name as departement "
 			+ " from h_log_transaction ph, personnel_employee pe, personnel_employee_area pae, personnel_department pd, personnel_position pp"
 			+ " where pp.id = pe.position_id and pd.id = pe.department_id and pae.employee_id = pe.id and ph.emp_code = pe.emp_code "
-			+ " and :endDate and ph.arrival_state = '3' and ph.log_date between :startDate order by ph.arrival_time desc limit 5", nativeQuery = true)
+			+ " and ph.arrival_state = '3' and ph.log_date between :startDate and :endDate order by ph.arrival_time desc limit 5", nativeQuery = true)
 	List<IPunchHistory> arrivalMax5(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
 
 	@Query(value = "select pe.emp_code as empCode, ph.arrival_time as arrivalTime, ph.arrival_id as arrivalId, ph.arrival_terminal_id as arrivalTerminalId,"
@@ -166,12 +167,12 @@ public interface PunchHistoryRepository extends JpaRepository<PunchHistory, Inte
 	@Query(value = "select count(ph.is_absent) from h_log_transaction ph where ph.is_absent = :state and ph.log_date between :startDate and :endDate AND ph.emp_code= :empCode", nativeQuery = true)
 	Integer countAbsentAndEmpCode(@Param("startDate") Date startDate, @Param("endDate") Date endDate, @Param("state") Boolean state,String empCode);
 
-	/*@Query(value = "SELECT hlt.id, pe.emp_code,pe.first_name, pe.gender, pe.email, pe.mobile, pe.last_name, STRING_AGG(pa.area_name, ','), hlt.arrival_time, hlt.arrival_state, hlt.departure_time, hlt.departure_state, hlt.presence_periode FROM h_log_transaction hlt\n" +
-			"INNER JOIN personnel_employee pe ON hlt.emp_code = pe.emp_code\n" +
-			"INNER JOIN personnel_employee_area pea ON pea.employee_id = pe.id\n" +
-			"INNER JOIN personnel_area pa ON pa.id = pea.area_id\n" +
-			"WHERE pe.emp_code = ':empCode'\n" +
-			"GROUP BY hlt.id, pe.emp_code,pe.first_name, pe.gender, pe.email, pe.mobile, pe.last_name")
-	List<Object[]> historyPointageByEmpCode(@Param("emp.code") String empCode);*/
+	/*@Query(value = "SELECT hlt.id as id , pe.emp_code as empCode ,pe.first_name, pe.gender, pe.email, pe.mobile, pe.last_name, STRING_AGG(pa.area_name, ','), hlt.arrival_time, hlt.arrival_state, hlt.departure_time, hlt.departure_state, hlt.presence_periode FROM h_log_transaction hlt\n" +
+			"INNER JOIN personnel_employee pe ON hlt.emp_code = pe.emp_code " +
+			"INNER JOIN personnel_employee_area pea ON pea.employee_id = pe.id " +
+			"INNER JOIN personnel_area pa ON pa.id = pea.area_id " +
+			"WHERE pe.emp_code = :empCode " +
+			"GROUP BY hlt.id, pe.emp_code,pe.first_name, pe.gender, pe.email, pe.mobile, pe.last_name",nativeQuery = true)
+	List<HistoryPointage>historyPointageByEmpCode(@Param("empCode") String empCode);*/
 
 }
